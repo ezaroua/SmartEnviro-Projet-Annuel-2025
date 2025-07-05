babfd1970fae39e3088d6f807ef880ebd17a29a2
Kevin CARTTIGUEANE <kevin.carttigueane@arkema.com>
2025-07-05 19:27:02 +0200

    Remove doc

--- backend/apps/prediction/apps.py
+++ backend/apps/prediction/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class PredictionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.prediction"
-
-
-# pluie le lendemain - binaire
